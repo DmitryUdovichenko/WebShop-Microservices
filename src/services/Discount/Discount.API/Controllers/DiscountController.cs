3e3cbba87c73561b9889a2c30d29bd6ebad26a4f
Dmitry Udovichenko <95741984+mbicycle-dmitry-udovichenko@users.noreply.github.com>
2022-07-07 23:28:07 +0300

    Add entiti, repository, controller

---
+++ src/services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,45 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{productId}", Name = "GetDiscount")]
+        public async Task<ActionResult<Coupon>> Get(string productId)
+        {
+            var coupon = await _repository.Get(productId);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Coupon>> Create(Coupon coupon)
+        {
+            await _repository.Create(coupon);
+            return CreatedAtRoute("GetDiscount", new { productId = coupon.ProductId }, coupon);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Coupon>> Update([FromBody] Coupon coupon)
+        {
+            return Ok(await _repository.Update(coupon));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _repository.Delete(id));
+        }
+    }
+}
