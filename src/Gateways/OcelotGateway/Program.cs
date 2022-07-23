4289ab2141ade51c947110394373adccdf2ceac0
Dmitry Udovichenko <dmitry.udovichenko@mbicycle.com>
2022-07-23 18:27:39 +0300

    add ocelot pathes and docker

--- src/Gateways/OcelotGateway/Program.cs
+++ src/Gateways/OcelotGateway/Program.cs
@@ -1,10 +1,26 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.AddJsonConsole();
-builder.Services.AddOcelot();
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Host.ConfigureLogging((hostingContext, logging) =>
+{
+    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    logging.AddConsole();
+    logging.AddDebug();
+});
+//builder.Logging.AddJsonConsole();
+
+builder.Services.AddOcelot().AddCacheManager(c =>
+{
+    c.WithDictionaryHandle();
+});
 
 var app = builder.Build();
 
