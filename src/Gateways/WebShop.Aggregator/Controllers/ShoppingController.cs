e5166d2270fadcd6de947d599a6d41707c6e7cd0
Dmitry Udovichenko <dmitry.udovichenko@mbicycle.com>
2022-07-24 16:11:46 +0300

    add web api aggregator

---
+++ src/Gateways/WebShop.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShop.Aggregator.Models;
+using WebShop.Aggregator.Services;
+
+namespace WebShop.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        [HttpGet("{userName}", Name = "GetShopping")]
+        public async Task<ActionResult<ShoppingModel>> Get(string userName)
+        {
+            var basket = await _basketService.GetBasket(userName);
+            foreach (var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalog(item.ProductId);
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            var orders = await _orderService.GetOrdersByUserName(userName);
+
+            var shoppingModel = new ShoppingModel
+            {
+                UserName = userName,
+                BasketWithProducts = basket,
+                Orders = orders
+            };
+
+            return Ok(shoppingModel);
+        }
+    }
+}
